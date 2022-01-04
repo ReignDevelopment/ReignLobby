b09442a4b41168752c93c61a200d08c81f199195
ImEcuadorian <hugosmm2000@gmail.com>
2022-01-04 18:40:45 -0500

    Create a new class for apply the color

---
+++ plugin/src/main/java/team/reign/lobby/util/ChatUtil.java
@@ -0,0 +1,12 @@
+package team.reign.lobby.util;
+
+import org.bukkit.ChatColor;
+
+
+public class ChatUtil {
+
+    public static String color (String string){
+        return ChatColor.translateAlternateColorCodes('&', string);
+    }
+
+}
