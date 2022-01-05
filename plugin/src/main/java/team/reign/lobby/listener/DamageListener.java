224330a5ff07f764b84914b100e455545fffab23
ImEcuadorian <hugosmm2000@gmail.com>
2022-01-05 08:12:40 -0500

    Change the method's listener

--- plugin/src/main/java/team/reign/lobby/listener/DamageListener.java
+++ plugin/src/main/java/team/reign/lobby/listener/DamageListener.java
@@ -1,23 +1,35 @@
 package team.reign.lobby.listener;
 
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityDamageByEntityEvent;
 import org.bukkit.event.entity.EntityDamageEvent;
 
+
 public class DamageListener implements Listener {
+
     @EventHandler(priority = EventPriority.HIGHEST)
-    public void onEntityDamageByEntity(EntityDamageByEntityEvent e) {
-        if (e.getEntity() instanceof org.bukkit.entity.Player)
-            e.setCancelled(true);
-        if (e.getDamager() instanceof org.bukkit.entity.Player)
-            e.setCancelled(true);
+    public void onEntityDamageByEntity(EntityDamageByEntityEvent event) {
+        if(event.getEntity() instanceof Player) {
+            event.setCancelled(true);
+        }
+        if(event.getDamager() instanceof Player) {
+            event.setCancelled(true);
+        }
     }
 
     @EventHandler(priority = EventPriority.HIGHEST)
-    public void onEntityDamage(EntityDamageEvent e) {
-        if (e.getEntity() instanceof org.bukkit.entity.Player && e.getCause() == EntityDamageEvent.DamageCause.FALL)
-            e.setCancelled(true);
+    public void onEntityDamage(EntityDamageEvent event) {
+        if(event.getEntity() instanceof Player) {
+            for(EntityDamageEvent.DamageCause damageCause : EntityDamageEvent.DamageCause.values()) {
+                if(event.getCause() == damageCause) {
+                    event.setCancelled(true);
+                }
+            }
+        }
+
     }
+
 }
